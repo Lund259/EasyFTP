3b49a053eb07f9ada46cc53108c9a3180f2905b8
Jonathan Lund <Jonathan.e.lund@gmail.com>
2020-04-04 13:08:19 +0200

    Add Service and Utility factories

---
+++ EasyFTPClient.Application/Foundation/Factories/ServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿using EasyFTPClient.Application.Foundation.Entity.Interfaces;
+using EasyFTPClient.Application.Foundation.Services;
+using EasyFTPClient.Application.Foundation.Services.Interfaces;
+using EasyFTPClient.Application.Foundation.Utilities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyFTPClient.Application.Foundation.Factories
+{
+    public class ServiceFactory
+    {
+        UtilityFactory utilityFactory = new UtilityFactory();
+
+        public IFTPService CreateFTPService(IConnectionProvider connectionProvider)
+        {
+            return new FTPService(utilityFactory.CreateRequestHandler(), connectionProvider);
+        }
+    }
+}
---
+++ EasyFTPClient.Application/Foundation/Factories/UtilityFactory.cs
@@ -0,0 +1,16 @@
+﻿using EasyFTPClient.Application.Foundation.Utilities;
+using EasyFTPClient.Application.Foundation.Utilities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyFTPClient.Application.Foundation.Factories
+{
+    public class UtilityFactory
+    {
+        public IRequestHandler CreateRequestHandler()
+        {
+            return new RequestHandler();
+        }
+    }
+}
