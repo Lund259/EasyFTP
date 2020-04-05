7f850633e342c34fbf0590c9215e5e579b84b0d2
Jonathan Lund <Jonathan.e.lund@gmail.com>
2020-04-05 12:02:22 +0200

    Add acquaintance namespace for common entities and interfaces

---
+++ EasyFTPClient.Application/Acquaintance/Connection.cs
@@ -0,0 +1,27 @@
+﻿using EasyFTPClient.Application.Acquaintance.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EasyFTPClient.Application.Acquaintance
+{
+    public class Connection : IConnectionProvider
+    {
+        public Uri Url { get; }
+
+        public ICredentials Credential { get; }
+
+        public Connection(Uri url, string username, string password)
+        {
+            Url = url;
+            Credential = new NetworkCredential(username, password);
+        }
+
+        public Connection(Uri url, NetworkCredential networkCredential)
+        {
+            Url = url;
+            Credential = networkCredential;
+        }
+    }
+}
---
+++ EasyFTPClient.Application/Acquaintance/FTPFileInfo.cs
@@ -0,0 +1,23 @@
+﻿using EasyFTPClient.Application.Acquaintance.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyFTPClient.Application.Acquaintance
+{
+    public class FTPFileInfo : IFTPFileInfo
+    {
+        public bool IsDirectory { get; }
+        public long FileSize { get; }
+        public DateTime LastModifiedDate { get; }
+        public string FileName { get; }
+
+        public FTPFileInfo(bool isDirectory, long fileSize, DateTime lastModifiedDate, string fileName)
+        {
+            IsDirectory = isDirectory;
+            FileSize = fileSize;
+            LastModifiedDate = lastModifiedDate;
+            FileName = fileName;
+        }
+    }
+}
