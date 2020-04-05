01451c9c10d91cb9cf6a86e8b4d0a5da3b56c8f0
Jonathan Lund <Jonathan.e.lund@gmail.com>
2020-04-05 13:00:59 +0200

    Add RequestHandler.GetResponseString() unit tests

--- Tests/EasyFTPClient.Application.Test/UnitTests/Foundation/RequestHandlerTests.cs
+++ Tests/EasyFTPClient.Application.Test/UnitTests/Foundation/RequestHandlerTests.cs
@@ -4,9 +4,11 @@ using EasyFTPClient.Application.Foundation.Entity;
 using EasyFTPClient.Application.Foundation.Entity.Interfaces;
 using EasyFTPClient.Application.Foundation.Factories;
 using EasyFTPClient.Application.Foundation.Utilities.Interfaces;
+using EasyFTPClient.Application.Test.UnitTests.Foundation.Mocks;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -73,6 +75,52 @@ namespace EasyFTPClient.Application.Test.UnitTests.Foundation
 
             //Assert
             Assert.DoesNotThrow(() => requestHandler.CreateFTPWebRequest(connection, path, requestMethod));
+        }
+
+        [Test]
+        public void GetResponseString_NullRequest_ThrowException()
+        {
+            //Arrange
+            IRequest request = null;
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => requestHandler.GetResponseStrings(request));
+        }
+
+        [Test]
+        public void GetResponseString_Success()
+        {
+            //Arrange
+            string[] expected = new string[]
+            {
+                "d--x--x--x    2 ftp      ftp          4096 Mar 07  2002 bin",
+                "-rw-r--r--    1 ftp      ftp        659450 Jun 15 05:07 TEST.TXT",
+                "-rw-r--r--    1 ftp      ftp      101786380 Sep 08  2008 TEST03-05.TXT"
+            };
+
+            //Simulate a responseStream from an FTP server
+            Stream responseStream = new MemoryStream();
+            StreamWriter sw = new StreamWriter(responseStream);
+
+            foreach(var line in expected)
+            {
+                sw.WriteLine(line);
+                sw.Flush();
+            }
+            responseStream.Seek(0, SeekOrigin.Begin);
+
+            ResponseMock responseMock = new ResponseMock(responseStream);
+            IRequest request = new RequestMock(responseMock);
+
+            //Act
+            var actual = requestHandler.GetResponseStrings(request);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.IsTrue(responseMock.CloseWasCalled);
+            Assert.IsTrue(responseMock.DisposeWasCalled);
         }
     }
 }
